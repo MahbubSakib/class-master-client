3a57146f76442c522e516ada9ed2f93c501fd51c
mahbubsakib <sakibmahbub92@gmail.com>
2025-01-14 18:22:10 +0600

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Router from './Routes/Router';
+import AuthProvider from './provider/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={Router}></RouterProvider>
+    <AuthProvider>
+      <RouterProvider router={Router}></RouterProvider>
+    </AuthProvider>
   </StrictMode>,
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#d97706',     // Amber
+        secondary: '#16a34a',   // Green
+        accent: '#dc2626',      // Red
+        background: '#fef3c7',  // Warm Light
+        text: '#4b5563',        // Cool Gray (Dark)
+      },
+    },
   },
   plugins: [
     require('daisyui'),
