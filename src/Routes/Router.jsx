c859eda9ad4d8d3421e2cd78e81d7261338188ad
mahbubsakib <sakibmahbub92@gmail.com>
2025-01-15 17:33:46 +0600

    dashboard

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,11 +1,11 @@
 import {
     createBrowserRouter,
-  } from "react-router-dom";
+} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
-  
+
 const Router = createBrowserRouter([
     {
         path: '/',
@@ -24,7 +24,22 @@ const Router = createBrowserRouter([
                 element: <Register></Register>
             },
         ]
-        
+
+    },
+    {
+        path: "/dashboard",
+        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
+        children: [
+            // admin route
+            {
+                path: '/dashboard/addItems',
+                element: <AdminRoute><AddItems></AddItems></AdminRoute>
+            },
+            // {
+            //     path: 'users',
+            //     element: <AllUsers></AllUsers>
+            // }
+        ]
     }
 ]);
 
---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import { FaAccusoft, FaBook, FaCalendar, FaEnvelope, FaHome, FaList, FaShoppingCart, FaUser, FaUtensils } from 'react-icons/fa';
+import { NavLink, Outlet } from 'react-router-dom';
+// import useCart from '../hooks/useCart';
+import useAdmin from '../hooks/useAdmin';
+
+const Dashboard = () => {
+    // const [cart] = useCart();
+    // get isAdmin value from database
+    const [isAdmin] = useAdmin();
+    return (
+        <div className='flex w-10/12'>
+            <div className='w-64 min-h-screen bg-orange-400'>
+                <ul className='menu p-4'>
+                    {
+                        isAdmin ? <>
+                            <li>
+                                <NavLink to={'/dashboard/adminHome'}>
+                                    <FaHome></FaHome>
+                                    Admin Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/addItems'}>
+                                    <FaUtensils></FaUtensils>
+                                    Add Items
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/manageItems'}>
+                                    <FaList></FaList>
+                                    Manage Items
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/manageBookings'}>
+                                    <FaBook></FaBook>
+                                    Manage Bookings
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/users'}>
+                                    <FaUser></FaUser>
+                                    All Users
+                                </NavLink>
+                            </li>
+                        </> : <>
+                            <li>
+                                <NavLink to={'/dashboard/userHome'}>
+                                    <FaHome></FaHome>
+                                    User Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/reservation'}>
+                                    <FaCalendar></FaCalendar>
+                                    Reservation
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/cart'}>
+                                    <FaShoppingCart></FaShoppingCart>
+                                    My Cart
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/review'}>
+                                    <FaAccusoft></FaAccusoft>
+                                    Add a Review
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/dashboard/bookings'}>
+                                    <FaList></FaList>
+                                    My Bookings
+                                </NavLink>
+                            </li>
+                        </>
+                    }
+
+
+                    <div className='divider'></div>
+                    <li>
+                        <NavLink to={'/'}>
+                            <FaHome></FaHome>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to={'/'}>
+                            <FaEnvelope></FaEnvelope>
+                            Contact
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+            <div className='flex-1 p-8'>
+                <Outlet>
+
+                </Outlet>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
